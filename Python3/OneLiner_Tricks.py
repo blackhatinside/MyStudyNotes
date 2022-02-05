bb920f9b160730948cbda79d4b779401a73bdacb
cyberkid05 <cyberadithya05@gmail.com>
2022-02-05 23:12:01 +0530

    Update OneLiner_Tricks.py

--- Python3/OneLiner_Tricks.py
+++ Python3/OneLiner_Tricks.py
@@ -13,3 +13,10 @@ res = "YES" if flag == 1 else "NO"  # res = "YES"
 
 # single line for loop
 arr = [(i + 1) * 10 for i in range(10)] # [10 20 30 ..... 80 90 100]
+
+# n-sized array declaration
+arr = [0] * 10  # [0 0 0 ..... 0 0 0]
+
+# filter in python array (single line for and if-else statement)
+oddarr = [i for i in range(10) if i % 2 == 1] # [1 3 5 7 9]
+

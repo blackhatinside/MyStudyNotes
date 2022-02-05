707b45a3fb2364654c95ace97f793696412c325d
cyberkid05 <cyberadithya05@gmail.com>
2022-02-05 23:09:09 +0530

    Update OneLiner_Tricks.py

--- Python3/OneLiner_Tricks.py
+++ Python3/OneLiner_Tricks.py
@@ -1,1 +1,15 @@
+# to get an array from STDIN
+arr = list(map(int, input().split()))
+# arr = [1, 3, 5, 7, 8, 6, 4, 2,]
 
+# to print the array without [,]
+print(*arr) # 1 3 5 ..... 6 4 2
+
+# to add seperator between different elements in the output array
+print(*arr, sep = "--->") # 1--->3--->5 ..... 6--->4--->2
+
+# single line if-else statement
+res = "YES" if flag == 1 else "NO"  # res = "YES"
+
+# single line for loop
+arr = [(i + 1) * 10 for i in range(10)] # [10 20 30 ..... 80 90 100]

afe79a29b04eed8c77234906cd435e5e20497528
cyberkid05 <cyberadithya05@gmail.com>
2022-02-06 09:12:25 +0530

    Update OneLiner_Tricks.py

--- Python3/OneLiner_Tricks.py
+++ Python3/OneLiner_Tricks.py
@@ -1,11 +1,14 @@
 # arr = [1, 3, 5, 7, 8, 6, 4, 2,]
-# grid = ['apple', 30], ['guava', 25], ['grapes', 25], ['banana', 20], ['orange', 40]]
+# grid = [['apple', 30], ['guava', 25], ['grapes', 25], ['banana', 20], ['orange', 40],]
 
 # to get an array from STDIN
 arr = list(map(int, input().split()))
 
 # to print the array without [,]
 print(*arr) # 1 3 5 ..... 6 4 2
+
+# to find the average of the array
+print(sum(arr) / len(arr))  # 4.5
 
 # to add seperator between different elements in the output array
 print(*arr, sep = "--->") # 1--->3--->5 ..... 6--->4--->2
@@ -26,5 +29,5 @@ oddarr = [i for i in range(10) if i % 2 == 1] # [1 3 5 7 9]
 diffarr = [abs(val - arr[i - 1]) for i, val in enumerate(arr, 0) if i > 0] # 2 2 2 1 2 2 2
 
 # sort a 2D array by its 2nd value first and then by its 1st value
-grid.sort(key = lambda x: (x[1], x[0]))
+grid.sort(key = lambda x:(x[1], x[0]))  # [['banana', 20], ['grapes', 25], ['guava', 25], ['apple', 30], ['orange', 40]]
 

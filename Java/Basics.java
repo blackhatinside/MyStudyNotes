a3cbb74afa84decf0400842a9377219edfb76e92
cyberkid05 <cyberadithya05@gmail.com>
2022-02-16 06:17:57 +0000

    Update Basics.java

--- Java/Basics.java
+++ Java/Basics.java
@@ -1,6 +1,8 @@
 /*
 
 Java is a verbose programming language.
+Java is platform independent.
+All code in Java is defined in classes.
 Compilation: Source Code to Byte Code (Creates a executable class file)
 {} can be used for creating scopes in the program
 

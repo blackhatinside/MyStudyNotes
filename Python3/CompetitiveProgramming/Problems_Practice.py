56b95e84aab0192ad09d066d081570cf77b35713
cyberkid05 <cyberadithya05@gmail.com>
2022-02-24 09:23:04 +0000

    Update Problems_Practice.py

--- Python3/CompetitiveProgramming/Problems_Practice.py
+++ Python3/CompetitiveProgramming/Problems_Practice.py
@@ -1,5 +1,6 @@
 # Important Problems for Practice for Interviews
 	# https://medium.com/techie-delight/stack-data-structure-practice-problems-and-interview-questions-9f08a35a7f19
+	# https://www.hackerearth.com/practice/data-structures/stacks/basics-of-stacks/practice-problems/
 
 # Stack: https://atcoder.jp/contests/agc005/tasks/agc005_a
 w = input(); stack = [];

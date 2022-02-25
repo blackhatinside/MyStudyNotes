5b7644ad48f36ff83ad2678dee0565b31d2141cc
cyberkid05 <cyberadithya05@gmail.com>
2022-02-25 19:32:37 +0530

    Update miniProject1.java

--- Java/miniProject1.java
+++ Java/miniProject1.java
@@ -25,6 +25,7 @@ import java.lang.*;
 import java.util.*;
 import java.util.Arrays;
 import java.util.HashMap;
+
 public class Application {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -160,6 +161,76 @@ public class Application {
 			}
 			case 5: {
 				System.out.println("Exiting from the program.");
+				choice = 0;
+				break;
+			}
+			default: {
+				System.out.println("Invalid Input!");
+			}
+			}
+		}
+	}
+}
+
+// ##################################################
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Application {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("\nMedia Player - Queue Implementation");
+		Date timenow = new Date();
+		System.out.println("Date: " + timenow.toString());
+		Queue<Song> playlist = new LinkedList<>();
+		Map<String, Integer> hp = new HashMap<>();
+		int choice = -1, currsize = 0;
+		while (choice != 0) {
+			System.out.println("\n1. Add a new Song\n2. Go to next Song\n3. What's the next song?\n4. Total number of Songs\n5. Exit Music Player\nEnter your choice: ");
+			choice = scan.nextInt(); scan.nextLine(); System.out.println();
+			switch (choice) {
+			case 1: {
+				currsize += 1;
+				// int songRank = currsize;
+				String songName = scan.next();
+				String singerName = scan.next();
+				boolean flag = true;
+				if (hp.containsKey(songName)) {
+					int y = hp.get(songName);
+					hp.put(songName, y + 1);
+				} else {
+					hp.put(songName, 0);
+				}
+				scan.nextLine();
+				if (hp.get(songName) == 0)
+					playlist.offer(new Song(songName, singerName));
+				System.out.println(playlist);
+				break;
+
+			}
+			case 2: {
+				if (playlist.size() == 0) {
+					System.out.println("Playlist is empty!");
+					break;
+				}
+				System.out.println("Removed: " + playlist.poll());
+				break;
+			}
+			case 3: {
+				if (playlist.size() == 0) {
+					System.out.println("That was the last song!");
+					break;
+				}
+				System.out.println(playlist.peek());
+				break;
+			}
+			case 4: {
+				System.out.println(playlist.size());
+				break;
+			}
+			case 5: {
 				choice = 0;
 				break;
 			}

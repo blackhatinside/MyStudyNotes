0a2a5c70301793a65ce9be53f4e2cd7c78a722e8
cyberkid05 <cyberadithya05@gmail.com>
2022-02-25 11:30:43 +0530

    Update and rename Python3/CompetitiveProgramming/Problems_Practice.java to DSA/Problems_Practice.java

---
+++ DSA/Problems_Practice.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main { // atcoder - Main; codechef - Main;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] w = scan.nextLine().toCharArray();
+		Stack<Character> stack = new Stack<>();
+		for (Character x : w) {
+			if ((stack.size() == 0) || !(Character.compare(stack.peek(), 'S') == 0 && Character.compare(x, 'T') == 0)) {
+				stack.push(x);
+			} else {
+				stack.pop();
+			}
+		}
+		int ans = stack.size();
+		System.out.println(ans);
+	}
+}
+

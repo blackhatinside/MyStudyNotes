5ec7ff15cb5eaa0b7bf9fb99ee33986b7b4f74b2
cyberkid05 <cyberadithya05@gmail.com>
2022-02-25 11:55:05 +0530

    Update miniProject1.java

--- Java/miniProject1.java
+++ Java/miniProject1.java
@@ -55,3 +55,73 @@ public class Application {
 		System.out.println(Student.average(csea.get(queryindex).getaverage));
 	}
 }
+
+// ##################################################
+
+// Vehicle.java
+public class Vehicle {
+	String username;
+	int vehicleID;
+	int wheeler;
+
+	Vehicle (String uname, int vID, int vWheeler) {
+		this.username = uname;
+		this.vehicleID = vID;
+		this.wheeler = vWheeler;
+	}
+	@Override
+	public String toString() {
+		return username + " " + wheeler + "-wheeler " + vehicleID;
+	}
+}
+
+// Applicationjava
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class Application {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Vehicle> lane1 = new Stack<>();
+		int choice = -1;
+		while (choice != 0) {
+			System.out.println("\nParking System\n1. Add a Vehicle\n2. Remove a Vehicle\n3. Number of Vehicles\n4. Peek into last Vehicle\n5. Exit\nEnter your choice: ");
+			choice = scan.nextInt(); scan.nextLine();
+			switch (choice) {
+			case 1: {
+				String givenName = scan.next();
+				int givenID = scan.nextInt();
+				int givenwheels = scan.nextInt();
+				scan.nextLine();
+				lane1.push(new Vehicle(givenName, givenID, givenwheels));
+				System.out.println("Vehicle added to lane!");
+				break;
+			}
+			case 2: {
+				System.out.println("Vehicle removed from lane!");
+				lane1.pop();
+				break;
+			}
+			case 3: {
+				System.out.println("Displaying lane details.");
+				System.out.println("\n" + lane1.size() + " vehicles found!\n");
+				System.out.println(lane1);
+				break;
+			}
+			case 4: {
+				System.out.println("Peeking into the last vehicle.");
+				System.out.println(lane1.peek());
+			}
+			case 5: {
+				System.out.println("Exiting from program.");
+				choice = 0;
+				break;
+			}
+			default: {
+				System.out.println("Invalid Input!");
+			}
+			}
+		}
+	}
+}

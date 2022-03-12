a582a48b2e13c7d5eb5ab3b33e498a61deb080a7
cyberkid05 <cyberadithya05@gmail.com>
2022-03-13 00:17:40 +0530

    Update Fundamentals.py

--- Algorithms_Python/Fundamentals.py
+++ Algorithms_Python/Fundamentals.py
@@ -23,3 +23,42 @@ def insertionSort(arr, n):  #arr is the array and n is the size of the array
             j -= 1 
         arr[j + 1] = cur
     return arr
+
+class Queue:    #queue implementation using Python list to help in BFS
+	queue = []
+	def __init__(self):
+		self.queue = []
+	def enqueue(self, x):
+		self.queue.append(x)
+	def dequeue(self):
+		return self.queue.pop(0)
+	def __len__(self):
+		return len(self.queue)
+class Graph:	# graph implementation using adjacency lists
+	V = 0	# number of vertices
+	l = [[], ]	# adjacency list
+	def __init__(self, v = 0):	# constructor
+		self.V = v
+		self.l = [list() for _ in range(self.V)]
+	def addEdge(self, i, j, undir = True):
+		self.l[i].append(j)
+		if undir:	# undirected graph has both A -> B and B -> A connections
+			self.l[j].append(i)
+	def BFS(self, graph, source):
+		visited = collections.defaultdict(lambda:False)
+		ans = list()
+		q = Queue()
+		q.enqueue(source)
+		visited[source] = True
+		while len(q) > 0:
+			ele = q.dequeue()
+			ans.append(ele)
+			for x in self.l[ele]:
+				if not visited[x]:
+					q.enqueue(x)
+					visited[x] = True
+		return ans
+	def printAdjList(self):
+		for ii in range(len(self.l)):
+			x = self.l[ii]
+			print(ii, *x)

3f8e1bc93b99742f69cfa9491f486acdf948be53
cyberkid05 <cyberadithya05@gmail.com>
2022-03-12 23:22:47 +0530

    What my brain failed to derive at - isVisited( ) ?

---
+++ Python3/Stooooopid_ideas/BFS2.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+
+class Queue:
+	queue = []
+	def __init__(self):
+		self.queue = []
+	def enqueue(self, x):
+		self.queue.append(x)
+	def dequeue(self):
+		return self.queue.pop(0)
+	def __len__(self):
+		return len(self.queue)
+
+class Graph:	# using adjacency lists
+	V = 0	# number of vertices
+	l = [[], ]	# adjacency list
+	def __init__(self, v = 0):	# constructor
+		self.V = v
+		self.l = [list() for _ in range(self.V)]
+
+	def addEdge(self, i, j, undir = True):
+		self.l[i].append(j)
+		if undir:	# undirected graph has both A -> B and B -> A connections
+			self.l[j].append(i)
+
+	def BFS(self, graph, source):
+		visited = defaultdict(lambda:False)
+		ans = list()
+		q = Queue()
+		q.enqueue(source)
+		visited[source] = True
+		while len(q) > 0:
+			ele = q.dequeue()
+			ans.append(ele)
+			for x in self.l[ele]:
+				if not visited[x]:
+					q.enqueue(x)
+					visited[x] = True
+		return ans
+
+	def printAdjList(self):
+		for ii in range(len(self.l)):
+			x = self.l[ii]
+			print(ii, *x)
+
+def main():
+	g = Graph(7)
+	g.addEdge(1, 2)
+	g.addEdge(1, 0)
+	g.addEdge(2, 3)
+	g.addEdge(0, 4)
+	g.addEdge(3, 5)
+	g.addEdge(4, 5)
+	g.addEdge(5, 6)
+	print(*g.BFS(g, 1))
+	return
+
+if __name__ == '__main__':
+	main()

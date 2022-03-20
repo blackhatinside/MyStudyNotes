a4df41edd8f9dc1f8e3b22da0e7ce1cf5cba4233
cyberkid05 <cyberadithya05@gmail.com>
2022-03-20 23:45:17 +0530

    Update Template_BFS.py

--- Python3/CompetitiveProgramming/Template_BFS.py
+++ Python3/CompetitiveProgramming/Template_BFS.py
@@ -11,32 +11,41 @@ class Queue:
 	def __len__(self):
 		return len(self.queue)
 
-class Graph:	# using adjacency lists
-	V = 0	# number of vertices
-	l = [[], ]	# adjacency list
-	def __init__(self, v = 0):	# constructor
-		self.V = v	# len(self.l)
-		self.l = [list() for _ in range(self.V)]
+class Graph:    # using adjacency lists
+    V = 0   # number of vertices
+    l = [[], ]  # adjacency list
+    def __init__(self, v = 0):  # constructor
+        self.V = v  # len(self.l)
+        self.l = [list() for _ in range(self.V)]
 
-	def addEdge(self, i, j, undir = True):
-		self.l[i].append(j)
-		if undir:	# undirected graph has both A -> B and B -> A connections
-			self.l[j].append(i)
+    def addEdge(self, i, j, undir = True):
+        self.l[i].append(j)
+        if undir:   # undirected graph has both A -> B and B -> A connections
+            self.l[j].append(i)
 
-	def BFS(self, graph, source):	# O(vertices + edges)
-		visited = defaultdict(lambda:False)
-		ans = list()
-		q = Queue()
-		q.enqueue(source)
-		visited[source] = True
-		while len(q) > 0:
-			ele = q.dequeue()
-			ans.append(ele)
-			for x in self.l[ele]:
-				if not visited[x]:
-					q.enqueue(x)
-					visited[x] = True
-		return ans
+    def BFS(self, graph, source):   # O(vertices + edges)
+        visited = defaultdict(lambda:False)
+        # visited = set()
+        ans = list()
+        q = Queue()
+        q.enqueue(source)
+        visited[source] = True
+        # visited.add(source)
+        while len(q) > 0:
+            ele = q.dequeue()
+            ans.append(ele)
+            for x in self.l[ele]:
+                if not visited[x]:
+                # if x not in visited:
+                    q.enqueue(x)
+                    visited[x] = True
+                    # visited.add(x)
+        return ans
+
+    def printAdjList(self):
+        for ii in range(self.V):
+            x = self.l[ii]
+            print(ii, *x)
 
 	def printAdjList(self):
 		for ii in range(self.V):

99f0179793481d52cfed22693bd6dc4e9b5ff552
cyberkid05 <cyberadithya05@gmail.com>
2022-03-30 15:16:24 +0530

    Update Dijkstra_Algo.java

--- Java/Dijkstra_Algo.java
+++ Java/Dijkstra_Algo.java
@@ -1,14 +1,10 @@
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Map.Entry;
-import java.util.Set;
-
-package com.baeldung.algorithms.ga.dijkstra;
-
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class Node {
 

bc816766b5bacd8d04b8df948724739d99c7f706
cyberkid05 <cyberadithya05@gmail.com>
2022-04-26 18:54:18 +0530

    Update Basics.cpp

--- Cpp/Basics.cpp
+++ Cpp/Basics.cpp
@@ -3,3 +3,14 @@ String str1 = "HelloWorld";
 str1.length();  // 10
 str1.substr(3); // str1[3:]
 str1.substr(0, 3);  // str1[0:3]
+
+int summ = 0;
+int a[] = { 5, 10, 15 };
+accumulate(a, a + 3, summ); // summ = 30
+
+int myfunc(int x, int y) {
+    return x * y;
+}
+int prod = 1;
+int a[] = { 5, 10, 15 };
+accumulate(a, a + 3, prod, myfunc); // prod = 750

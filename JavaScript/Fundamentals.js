67a51fe840f1e452e79478666dc917a0b5278a00
cyberkid05 <cyberadithya05@gmail.com>
2022-05-24 17:39:04 +0530

    Update Fundamentals.js

--- JavaScript/Fundamentals.js
+++ JavaScript/Fundamentals.js
@@ -1,3 +1,17 @@
 // # use let to reassign variables
+let x = 10
 
 // # use const to make variable constant
+const pi = 3.14
+
+// # for loop
+for (let i = 0; i < n; i++) {
+
+}
+
+// # declare an array of size n with all values as 0
+let arr = Array(n).fill(0)
+
+// # length of the array
+let sz = arr.length
+

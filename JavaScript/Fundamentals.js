6d04978531e49e0778a4825726a47c1813616b9f
cyberkid05 <cyberadithya05@gmail.com>
2022-05-25 11:38:45 +0530

    Update Fundamentals.js

--- JavaScript/Fundamentals.js
+++ JavaScript/Fundamentals.js
@@ -38,3 +38,20 @@ var inorderTraversal = function(root) {
 
 // # concatenation of 2 arrays
 c = [...a,...b]
+
+// # technique: binary search
+var search = function(nums, target) {
+    let beg = 0, end = nums.length - 1
+    while (beg <= end) {
+        mid = beg + ~~((end - beg) / 2)
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] < target) {
+            beg = mid + 1
+        }
+        else {
+            end = mid - 1
+        }
+    }
+    return -1;
+};

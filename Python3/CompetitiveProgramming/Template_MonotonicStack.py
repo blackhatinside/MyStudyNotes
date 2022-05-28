152ef404390eb71df6341c53d56733250f5ae0e7
cyberkid05 <cyberadithya05@gmail.com>
2022-05-28 08:45:10 +0530

    Update Template_MonotonicStack.py

--- Python3/CompetitiveProgramming/Template_MonotonicStack.py
+++ Python3/CompetitiveProgramming/Template_MonotonicStack.py
@@ -1,6 +1,6 @@
 class Solution:
     def monotonicstack(self, arr, n):
-        ''' Returns the index of NGE'''
+        ''' Returns the index of NGE '''
         stack = []  # monotonic stack
         nge = [0] * n
         for i in range(n - 1, -1, -1):
@@ -39,4 +39,23 @@ class Solution:
             else:
                 ans.append(-1)
         return ans
-      
+
+'''---------- https://leetcode.com/problems/next-greater-element-ii/ ----------'''
+
+class Solution:
+    def monotonicstack(self, arr, n):
+        stack = []  # monotonic stack
+        nge = [0] * n
+        for i in range(2 * n - 1, -1, -1):
+            while stack != [] and arr[i % n] >= arr[stack[-1]]:
+                stack.pop()
+            if stack == []:
+                nge[i % n] = -1
+            else:
+                nge[i % n] = stack[-1]
+            stack.append(i % n)
+        return nge
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        greater = self.monotonicstack(nums, len(nums))
+        ans = [nums[greater[i]] if greater[i] != -1 else -1 for i in range(len(nums))]
+        return ans

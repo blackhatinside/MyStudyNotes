54814b1e74371905b679d3b957fedb966f7d7afa
cyberkid05 <cyberadithya05@gmail.com>
2022-06-02 22:42:59 +0530

    Update Template_MonotonicStack.py

--- Python3/CompetitiveProgramming/Template_MonotonicStack.py
+++ Python3/CompetitiveProgramming/Template_MonotonicStack.py
@@ -4,13 +4,13 @@ class Solution:
         stack = []  # monotonic stack
         nge = [0] * n
         for i in range(n - 1, -1, -1):
-            while stack != [] and arr[i] >= arr[stack[-1]]:
+            while stack != [] and arr[i % n] >= arr[stack[-1]]:
                 stack.pop()
             if stack == []:
-                nge[i] = -1
+                nge[i % n] = -1
             else:
-                nge[i] = stack[-1]
-            stack.append(i)
+                nge[i % n] = stack[-1]
+            stack.append(i % n)
         return nge
 # greater = self.monotonicstack(arr, n)
 

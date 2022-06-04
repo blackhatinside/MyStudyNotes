0bd873d0628b29ebc5fc82acd75b87814d24ce4c
cyberkid05 <cyberadithya05@gmail.com>
2022-06-04 09:20:11 +0530

    Update Template_Array.py

--- Python3/CompetitiveProgramming/Template_Array.py
+++ Python3/CompetitiveProgramming/Template_Array.py
@@ -13,3 +13,17 @@ class Solution:
             localmax = max(nums[i], localmax + nums[i])
             globalmax = max(globalmax, localmax)
         return globalmax
+    
+class Solution:
+    def maxProduct(self, arr: List[int]) -> int:
+        max_value=arr[0]
+        min_value=arr[0]
+        ans=arr[0]
+        for i in range(1,len(arr)):                 #using DP approach
+            choice1=arr[i]*max_value                # + for subarraysum, * for subarrayproduct
+            choice2=arr[i]*min_value                # + for subarraysum, * for subarrayproduct
+            # if -ve ele then min +/* ele, else max +/* ele, also compare ele alone by itself
+            max_value=max(arr[i],choice1,choice2)   # local maximum
+            min_value=min(arr[i],choice1,choice2)   # local minimum
+            ans=max(ans,max_value)                  # global maximum
+        return(ans)

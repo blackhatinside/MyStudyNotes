55729e1167e4e2777db08a6713993ada2229a998
cyberkid05 <cyberadithya05@gmail.com>
2022-06-04 09:44:47 +0530

    Update Template_Array.py

--- Python3/CompetitiveProgramming/Template_Array.py
+++ Python3/CompetitiveProgramming/Template_Array.py
@@ -1,6 +1,6 @@
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
-        maxx, minn = -1, nums[0]
+        maxx, minn = 0, nums[0]
         for i in range(len(nums)):
             maxx = max(maxx, nums[i] - minn)
             minn = min(minn, nums[i])

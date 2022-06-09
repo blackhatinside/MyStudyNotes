6eabb8f474c1425d0c2cb57251778d41b0b8fb5c
cyberkid05 <cyberadithya05@gmail.com>
2022-06-09 20:35:28 +0530

    Update Template_GraphDFS.py

--- Python3/CompetitiveProgramming/Template_GraphDFS.py
+++ Python3/CompetitiveProgramming/Template_GraphDFS.py
@@ -1,14 +1,14 @@
-class Graph:	# using adjacency lists
-	V = 0	# number of vertices
-	l = [[], ]	# adjacency list
-	def __init__(self, v = 0):	# constructor
-		self.V = v
-		self.l = [list() for _ in range(self.V)]
+class Graph:    # simple graph using adjacency lists
+    V = 0   # number of vertices (0 to v - 1 vertices)
+    ADJ = []    #adjacency list
+    def __init__(self, v):
+        self.V = v
+        self.ADJ = [[] for _ in range(self.V)]
 
-	def addEdge(self, i, j, undir = True):
-		self.l[i].append(j)
-		if undir:	# undirected graph has both A -> B and B -> A connections
-			self.l[j].append(i)
+    def addEdge(self, i, j, undir = True):	# undirected graph
+        self.ADJ[i].append(j)
+        if undir:
+            self.ADJ[j].append(i)
 
     def dfs(self, src):
         dfs_path = []
@@ -23,18 +23,21 @@ class Graph:	# using adjacency lists
         dfsUtil(src)
         print(*dfs_path)
 
+    def printAdjList(self):
+        for i in range(self.V):
+            print("{} : {}".format(i, self.ADJ[i]))
+
 def main():
-	g = Graph(7)
-	g.addEdge(0, 1)
-	g.addEdge(1, 2)
-	g.addEdge(2, 3)
-	g.addEdge(3, 5)
-	g.addEdge(5, 6)
-	g.addEdge(4, 5)
-	g.addEdge(0, 4)
-	g.addEdge(3, 4)
-	print(g.DFS(0))
-	return
+    g = Graph(7)
+    g.addEdge(0, 1)
+    g.addEdge(1, 2)
+    g.addEdge(2, 3)
+    g.addEdge(3, 5)
+    g.addEdge(5, 6)
+    g.addEdge(4, 5)
+    g.addEdge(0, 4)
+    g.addEdge(3, 4)
+    g.dfs(1)
 
 if __name__ == '__main__':
-	main()
+    main()

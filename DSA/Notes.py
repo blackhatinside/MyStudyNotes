9ef00221b164b0110dad9a5dbd8cb3f2c808a4e8
cyberkid05 <cyberadithya05@gmail.com>
2022-08-07 09:34:56 +0530

    Update Notes.py

--- DSA/Notes.py
+++ DSA/Notes.py
@@ -1,1 +1,10 @@
 
+'''
+Data Structures
+    - Non Primitive
+        * Linear
+            # Static - Arrays
+            # Dynamic - Linked Lists, Stacks, Queues
+        * Non Linear - Trees, Graphs
+    - Primitive - Int, Char, String, Boolean
+'''

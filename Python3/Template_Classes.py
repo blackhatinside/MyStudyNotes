4e3f83bae92ea15466f7795b17fcb5b3643fef04
cyberkid05 <cyberadithya05@gmail.com>
2022-08-07 17:15:21 +0530

    Update Template_Classes.py

--- Python3/Template_Classes.py
+++ Python3/Template_Classes.py
@@ -11,9 +11,14 @@ class Queue:
 
 class LinkedListNode:
     # __slots__ = ['data', 'next',]
-    def __init__(self, data):
+    def __init__(self, data = None):
         self.data = data
         self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
 class TreeNode:
     def __init__(self, data):
